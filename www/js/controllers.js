bc2149ec753c16888d6be96be86fdbfd29d60825
ismailas <ismail.a.shobar@gmail.com>
2016-07-19 10:18:44 +0700

    baru

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,11 +9,41 @@ angular.module('app.controllers', [])
 })
 
 .controller('menuCtrl', function($scope) {
+    // username
     $scope.name = "Sumail Abdush";
+    $scope.statuscheck  = true;
 
     $scope.changeName   = function() {
         $scope.name = "randomed" + Math.random();
     }
+    // foto profil
+    $scope.pimg = "img/fotoprofil.jpg";
+
+    $scope.changePimg = function(){
+
+    };
+    // status donor
+
+    $scope.status = "ready";
+
+    $scope.changeStatus = function() {
+        if (today - user.lastdonate >= 90) {
+          $scope.status = "ready";
+        }
+        else {
+          $scope.status = today - user.lastdonate + "left";
+        }
+    };
+
+    // golongan darah
+    $scope.goldar = "A";
+
+    $scope.changeGoldar = function(){};
+
+    // jumlah donasi
+    $scope.jumdon = "3";
+
+    $scope.changeJumdon = function(){};
 })
 
 .controller('profileCtrl', function($scope) {
